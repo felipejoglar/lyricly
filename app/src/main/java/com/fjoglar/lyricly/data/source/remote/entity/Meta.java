7d051b844357396ddb3580e9d80f240c70501441
Felipe Joglar <felipejoglar@gmail.com>
2018-07-04 20:02:55 +0200

    feat: Add remote data entities

---
+++ app/src/main/java/com/fjoglar/lyricly/data/source/remote/entity/Meta.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 Felipe Joglar Santos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fjoglar.lyricly.data.source.remote.entity;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Meta {
+
+    @SerializedName("totalCount")
+    @Expose
+    private int totalCount;
+    @SerializedName("returnedCount")
+    @Expose
+    private int returnedCount;
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public int getReturnedCount() {
+        return returnedCount;
+    }
+
+    public void setReturnedCount(int returnedCount) {
+        this.returnedCount = returnedCount;
+    }
+
+}
