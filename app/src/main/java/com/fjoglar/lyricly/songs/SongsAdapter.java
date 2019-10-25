9a7531504d8977237cc9bf191d04a43d71225898
Felipe Joglar <felipejoglar@gmail.com>
2019-10-25 02:01:50 +0200

    Rename helper class

--- app/src/main/java/com/fjoglar/lyricly/songs/SongsAdapter.java
+++ app/src/main/java/com/fjoglar/lyricly/songs/SongsAdapter.java
@@ -29,7 +29,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.fjoglar.lyricly.R;
 import com.fjoglar.lyricly.data.model.Song;
-import com.fjoglar.lyricly.util.GlideHelper;
+import com.fjoglar.lyricly.util.ImageLoaderHelper;
 
 import java.util.List;
 
@@ -60,10 +60,10 @@ public class SongsAdapter extends RecyclerView.Adapter<SongsAdapter.SongViewHold
     @Override
     public void onBindViewHolder(@NonNull SongViewHolder holder, int position) {
         String albumId = mSongs.get(position).getAlbumId();
-        String imageUrl = mContext.getString(R.string.album_image_url, albumId, GlideHelper.IMAGE_SIZE_BIG);
-        String thumbUrl = mContext.getString(R.string.album_image_url, albumId, GlideHelper.IMAGE_SIZE_SMALL);
+        String imageUrl = mContext.getString(R.string.album_image_url, albumId, ImageLoaderHelper.IMAGE_SIZE_BIG);
+        String thumbUrl = mContext.getString(R.string.album_image_url, albumId, ImageLoaderHelper.IMAGE_SIZE_SMALL);
 
-        GlideHelper.loadWithThumb(holder.imageViewAlbumCover, imageUrl, thumbUrl);
+        ImageLoaderHelper.loadWithThumb(holder.imageViewAlbumCover, imageUrl, thumbUrl);
     }
 
     @Override
